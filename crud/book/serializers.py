e863e0e5954f0394c99e546340b10b9743c61763
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-07-19 15:27:51 +0100

    Updated book serializer to accept creation of new entry

--- crud/book/serializers.py
+++ crud/book/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only = True)
+    title = serializers.CharField()
+    author = serializers.CharField()
+    year = serializers.DateField()
+
     class Meta:
         model = Book
-        fields = ('id', 'title', 'author', 'year')
+        fields = ("id", "title", "author", "year")
+
+    def create(self, validated_data):
+        return Book.objects.create(**validated_data)
