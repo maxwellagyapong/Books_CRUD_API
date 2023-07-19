58cc2e99d57b8c148f3c73cff5ac3b2d1a2a099e
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-07-19 12:38:34 +0100

    Added book model

--- crud/book/models.py
+++ crud/book/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-
+class Book(models.Model):
+    title = models.CharField(_("Book Title"), max_length=100, unique=True, blank=False, null=False)
+    author = models.CharField(_("Author"), max_length=100, unique=False, blank=False, null=False)
+    year = models.DateField(_("Year Published"), auto_now=False, auto_now_add=False)
