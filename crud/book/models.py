4e1422c5920e42598c5425ba99e302dd131bd0bb
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-07-19 12:42:15 +0100

    Updated book model

--- crud/book/models.py
+++ crud/book/models.py
@@ -5,3 +5,6 @@ class Book(models.Model):
     title = models.CharField(_("Book Title"), max_length=100, unique=True, blank=False, null=False)
     author = models.CharField(_("Author"), max_length=100, unique=False, blank=False, null=False)
     year = models.DateField(_("Year Published"), auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.title
