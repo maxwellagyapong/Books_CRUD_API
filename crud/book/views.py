331c5725fae89ad57dd751309c82e61ea2d64d44
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-07-19 15:03:09 +0100

    Added book list / create view

--- crud/book/views.py
+++ crud/book/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Book
+from .serializers import BookSerializer
 
+@api_view(["GET", "POST"])
+def book_list(request):
 
+    if request.method == "GET":
+        try:
+            books = Book.objects.all()
+        except Book.DoesNotExist:
+            raise("Books not found!")
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+    
+    if request.method == "POST":
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
