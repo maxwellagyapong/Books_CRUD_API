a86a251358c1c31af738f21827e0d93dea8de28f
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-07-21 11:13:09 +0100

    Updated Book Model

--- crud/book/models.py
+++ crud/book/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class Book(models.Model):
-    title = models.CharField(_("Book Title"), max_length=100, unique=True, blank=False, null=False)
-    author = models.CharField(_("Author"), max_length=100, unique=False, blank=False, null=False)
-    year = models.IntegerField(_("Year Published"),)
+    title = models.CharField(_("Book Title"), max_length=100, unique=True)
+    author = models.CharField(_("Author"), max_length=100, unique=False)
+    year = models.IntegerField(_("Year Published"))
 
     def __str__(self):
         return self.title
